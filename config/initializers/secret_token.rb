bd7f372f3eba237bf50207a86e393421d4bcb8b9
Ronnie Nyaga <rnafundi@gmail.com>
2013-10-02 09:30:00 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviemania20::Application.config.secret_key_base = 'b4968244eb4f81f314ed1dc9371836a40f70232ed37101bd624fb678e7e1e0af0139f6f2375b1e60189b941c4a968a85ff80d884ec2fe88c1759dfbe5019fe90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moviemania20::Application.config.session_store :cookie_store, key: '_moviemania2_0_session'
