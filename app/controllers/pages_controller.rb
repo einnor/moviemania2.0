a79d15606d37da8c2eba387dd15aee0d230171fa
Ronnie Nyaga <rnafundi@gmail.com>
2013-10-02 19:43:34 +0300

    Finished styling the header partial

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  def home
+  	@title = "Home"
+  end
+
+  def contacts
+  	@title = "Contacts"
+  end
+
+  def about
+  	@title = "About"
+  end
+
+  def help
+  	@title = "Help"
+  end
+
+  def contactus
+  	@title = "Contact Us"
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get contacts" do
+    get :contacts
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+  end
+
+  test "should get contactus" do
+    get :contactus
+    assert_response :success
+  end
+
+end
