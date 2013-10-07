e896fe71ce46dacdca0c10093c06b086ad29320e
Ronnie Nyaga <rnafundi@gmail.com>
2013-10-07 12:43:09 +0300

    Added a Comingsoon new page

--- app/models/comingsoon.rb
+++ app/models/comingsoon.rb
@@ -1,4 +1,27 @@
+# == Schema Information
+#
+# Table name: comingsoons
+#
+#  id              :integer          not null, primary key
+#  title           :string(255)
+#  synopsis        :text
+#  category        :string(255)
+#  youtubestub     :string(255)
+#  cinemashowing   :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  comingsooncover :string(255)
+#
+
 class Comingsoon < ActiveRecord::Base
-mount_uploader :comingsooncover, ComingsooncoverUploader
+	mount_uploader :comingsooncover, ComingsooncoverUploader
+
+	validates :title,	presence: true,
+						length: {maximum: 50}
+	validates :synopsis,	presence: true
+	validates :category,	presence: true
+	validates :youtubestub,	presence: true
+	validates :cinemashowing,	presence: true
+	validates :comingsooncover,	presence: true
 
 end
